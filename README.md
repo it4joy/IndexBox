13faba86ff57a4fc087b9e8e82dcdf4a924aa3b3
Igor Ivanov <drkierkegor@gmail.com>
2018-11-29 19:16:54 +0300

    sm basic css updt

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Test task
 ### Project Features  
 * No Bootstrap
 * Webpack v.4
+* SCSS and components
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 import $ from 'jquery';
-import popper from 'popper.js';
-import bootstrap from 'bootstrap';
 
 import './js/main';
 import './scss/main.scss';
